31cdc03343bf902b7bc026190c5b8dec943e22a2
soojin <yunsoojini@hyundai-autoever.com>
2020-07-02 14:06:49 +0900

    [#173302101] Change ID of ApiAccess to auto-generated UUID

--- src/main/java/com/hyundaiautoever/ccs/metering/ApiAccess.java
+++ src/main/java/com/hyundaiautoever/ccs/metering/ApiAccess.java
@@ -7,24 +7,26 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @Data
 @Entity
-@IdClass(ApiAccessId.class)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiAccess {
+
     @Id
+    @GeneratedValue
+    private UUID id;
+
     private String handPhoneId;
 
-    @Id
     private String carId;
 
-    @Id
     private String requestUrl;
 
     @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
--- src/main/java/com/hyundaiautoever/ccs/metering/ApiAccessRepository.java
+++ src/main/java/com/hyundaiautoever/ccs/metering/ApiAccessRepository.java
@@ -3,6 +3,8 @@ package com.hyundaiautoever.ccs.metering;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ApiAccessRepository extends JpaRepository<ApiAccess,ApiAccessId > {
+public interface ApiAccessRepository extends JpaRepository<ApiAccess, UUID> {
 }
