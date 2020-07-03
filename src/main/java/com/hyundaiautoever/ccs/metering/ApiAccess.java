d5084ad0acdb0c4a2e1c1a62632cdad16c62549d
soojin <yunsoojini@hyundai-autoever.com>
2020-07-03 14:35:50 +0900

    [#173302101] add Indexes for api_access table to make query faster

--- src/main/java/com/hyundaiautoever/ccs/metering/ApiAccess.java
+++ src/main/java/com/hyundaiautoever/ccs/metering/ApiAccess.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -17,6 +14,12 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(indexes = {
+        @Index(
+                name = "apiaccess_handphoneid_carid_requesturl",
+                columnList = "handphoneid,carid,requesturl"
+        )
+})
 public class ApiAccess {
 
     @Id
