be301c7e534550e53f43699838f2994c492369fa
Sangrok Mun <rocky@hyundai-autoever.com>
2020-07-30 10:16:41 +0900

    [#173934346] Move TID handling to library
    Signed-off-by: Alan Shelly <ashelly@vmware.com>

--- src/main/java/com/hyundaiautoever/ccs/metering/MeteringApplication.java
+++ src/main/java/com/hyundaiautoever/ccs/metering/MeteringApplication.java
@@ -1,12 +1,15 @@
 package com.hyundaiautoever.ccs.metering;
 
+import com.hyundaiautoever.transactionlogger.TransactionLoggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 import java.time.Clock;
 
 @SpringBootApplication
+@Import(TransactionLoggerConfiguration.class)
 public class MeteringApplication {
 
     public static void main(String[] args) {
