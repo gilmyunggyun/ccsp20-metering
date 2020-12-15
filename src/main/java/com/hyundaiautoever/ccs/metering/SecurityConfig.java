95606bac6d8ca4a4c5de0b8e628180b44656ccfc
soojin <yunsoojini@hyundai-autoever.com>
2020-12-15 15:14:48 +0900

    add securityconfig

--- src/main/java/com/hyundaiautoever/ccs/metering/SecurityConfig.java
+++ src/main/java/com/hyundaiautoever/ccs/metering/SecurityConfig.java
@@ -1,28 +1,44 @@
 package com.hyundaiautoever.ccs.metering;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final Environment env;
+
+    @Autowired
+    public SecurityConfig(Environment env) {
+        this.env = env;
+    }
+
+    private static String PRD = "prd";
+    private static String ENV_PATH = "/actuator/env";
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       // http.authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated().and().csrf().disable();
-//        http.authorizeRequests().anyRequest().permitAll().and().httpBasic().disable().csrf().disable();
-        http
-                .authorizeRequests()
-                .antMatchers("/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .disable()
-                .csrf()
-                .disable();
-    }  
-     }
+        if (StringUtils.equalsAny(PRD, env.getActiveProfiles())) {
+            http.authorizeRequests()
+                    .antMatchers(ENV_PATH).authenticated()
+                    .anyRequest().permitAll()
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable();
+        } else {
+            http.authorizeRequests()
+                    .anyRequest().permitAll()
+                    .and()
+                    .httpBasic().disable()
+                    .csrf().disable();
+
+        }
+    }
+}
