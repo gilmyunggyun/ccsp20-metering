b3351e6f58b1d05dcf5ebb51429de7b3d15f2200
soojin <yunsoojini@hyundai-autoever.com>
2020-12-18 17:51:53 +0900

    add EnablePropertyEncrypt 적용

---
+++ src/test/java/com/hyundaiautoever/ccs/metering/EncryptCheckTests.java
@@ -0,0 +1,24 @@
+package com.hyundaiautoever.ccs.metering;
+
+import ccs.core.data.encrypt.EnablePropertyEncrypt;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@EnablePropertyEncrypt
+public class EncryptCheckTests {
+    @Autowired
+    StringEncryptor jasyptStringEncryptor;
+
+    @Test
+    void jasyptEncrypt(){
+        String data = "devOps1!";
+        String enc = jasyptStringEncryptor.encrypt(data);
+        System.err.println(enc);
+        System.err.println(jasyptStringEncryptor.decrypt(enc));
+    }
+
+}
