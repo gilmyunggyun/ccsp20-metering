27f50d1d66ad0767fdff30f18b098b4f642e7dcd
soojin <yunsoojini@hyundai-autoever.com>
2020-12-21 21:34:46 +0900

    add securityconfig 12/21

--- src/main/java/com/hyundaiautoever/ccs/metering/SecurityConfig.java
+++ src/main/java/com/hyundaiautoever/ccs/metering/SecurityConfig.java
@@ -1,42 +1,47 @@
 package com.hyundaiautoever.ccs.metering;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    private final Environment env;
-
-    @Autowired
-    public SecurityConfig(Environment env) {
-        this.env = env;
-    }
-
-    private static String PRD = "prd";
     private static String ENV_PATH = "/actuator/env";
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        if (StringUtils.equalsAny(PRD, env.getActiveProfiles())) {
-            http.authorizeRequests()
-                    .antMatchers(ENV_PATH).authenticated()
-                    .anyRequest().permitAll()
-                    .and()
-                    .httpBasic().disable()
-                    .csrf().disable();
-        } else {
-            http.authorizeRequests()
-                    .anyRequest().permitAll()
-                    .and()
-                    .httpBasic().disable()
-                    .csrf().disable();
+    private static String SERVICE_PATH = "/metering/**";
 
+    @Configuration
+    public static class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher(SERVICE_PATH).authorizeRequests()
+                    .anyRequest().permitAll()
+                    .and()
+                    .httpBasic().disable()
+                    .csrf().disable();
+        }
+
+    }
+
+
+    @Configuration
+    @Profile("prd")
+    @Order(1)
+    public static class ActuatorConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher(ENV_PATH).authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable();
         }
     }
 }
