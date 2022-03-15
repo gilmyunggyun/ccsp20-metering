7e4a625e170394ace8990024de6204a9a32e3696
ryuchangsung <groove@hyundai-autoever.com>
2022-03-15 13:17:15 +0900

    id "com.hkmc.h2" version "1.0.16" 로깅 적용됨 ValidationFilter 없으면 동작 안함

---
+++ src/main/java/com/hkmc/ccs/metering/config/ValidationFilter.java
@@ -0,0 +1,31 @@
+package com.hkmc.ccs.metering.config;
+
+import com.hkmc.filter.wrapper.MultiReadHttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Slf4j
+public class ValidationFilter extends GenericFilterBean {
+
+    private String validationCheck;
+
+	public ValidationFilter(final String validationCheck) {
+		this.validationCheck = validationCheck;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = ((HttpServletRequest) request);
+		MultiReadHttpServletRequest wrapper = new MultiReadHttpServletRequest(httpRequest);
+		chain.doFilter(wrapper, response);
+	}
+
+}
+
