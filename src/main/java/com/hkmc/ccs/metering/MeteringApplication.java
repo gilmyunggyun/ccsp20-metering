ccf152b13ec0b483b0f09bde16ee3c88db352bc3
1136314 <kilmmk@hyundai-autoever.com>
2023-05-22 18:19:49 +0900

    mac test

--- src/main/java/com/hkmc/ccs/metering/MeteringApplication.java
+++ src/main/java/com/hkmc/ccs/metering/MeteringApplication.java
@@ -7,7 +7,9 @@ import java.time.Clock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
@@ -15,6 +17,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 //@EnablePropertyEncrypt
 //@EnableTransactionLogger
+@EntityScan("com.hkmc.ccs.metering.models.entity")
 public class MeteringApplication {
 
   public static void main(String[] args) {
